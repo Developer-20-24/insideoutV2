0f359534a64a7d2e488d1ef8e19c44cdec4db40a
AngieHdez <ahernandez46@udi.edu.co>
2024-10-24 20:46:01 -0500

    24/10/2024

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,26 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import re
+
+
+class ActionValidateInput(Action):
+    def name(self) -> Text:
+        return "action_validate_input"
+
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+           
+        user_message = tracker.latest_message.get('text', '')
+       
+        if re.search(r'\d', user_message):
+            dispatcher.utter_message(text="Lo siento, no puedo procesar números ni secuencias numéricas. Por favor, hazme una pregunta sobre nuestros servicios usando palabras.")
+            return []
+           
+        return []
